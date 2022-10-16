11e88efddca3cfb802182c8ade82f302657c7d81
aa <49025536+anibal-aguila@users.noreply.github.com>
2022-10-16 13:41:41 +0200

    Update init.lua

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,3 @@
 -- example file i.e lua/custom/init.lua
 
 -- load your globals, autocmds here or anything .__.
-
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim' -- this is essential.
-end)
